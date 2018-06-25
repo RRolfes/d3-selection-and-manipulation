b2b5367350554078308a098eccd34fdd64af489c
Ryan Rolfes <ryanmrolfes@gmail.com>
2018-06-24 18:12:58 -0700

    adds more paragraphs and mutates nodes

--- index.js
+++ index.js
@@ -6,3 +6,13 @@ d3.select('body')
 .append('p')
 .text('First paragraph added')
 .attr('id', 'p1');
+
+d3.select('body')
+  .append('p')
+  .text('Second paragraph added');
+
+d3.select('body')
+  .append('p')
+  .text('Third paragraph added');
+
+d3.selectAll('p').style('color', 'blue')
